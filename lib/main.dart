36c1a92238c349a34e9611d8a3e415a04f6a217b
Umer Khan <umerkhan@Umers-MacBook-Pro.local>
2020-12-29 10:22:31 +0200

    adding questionsL=

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,33 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+  void answerChosen() {
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    var questions = [
+      'What\'s your favourite color ?',
+      'What\'s your favourite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("MY Text is here"),
-        ),
-        body: Text("Hey this is my body text"),
-      ),
+          appBar: AppBar(
+            title: Text("MY Text is here"),
+          ),
+          body: Column(
+            children: [
+              Text(
+                questions[questionIndex],
+              ),
+              RaisedButton(child: Text("Answer 1"), onPressed: answerChosen),
+              RaisedButton(child: Text("Answer 2"), onPressed: answerChosen),
+              RaisedButton(child: Text("Answer 3"), onPressed: answerChosen),
+            ],
+          )),
     );
   }
 }
