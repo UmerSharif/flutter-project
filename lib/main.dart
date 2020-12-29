75a3c2687ce8efb642a56774de4a08cb8e4ae282
Umer Khan <umerkhan@Umers-MacBook-Pro.local>
2020-12-29 17:54:40 +0200

    add stateful widget
    commit by UmerSk

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,19 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
   var questionIndex = 0;
   void answerChosen() {
-    questionIndex = questionIndex + 1;
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
     print(questionIndex);
   }
 
